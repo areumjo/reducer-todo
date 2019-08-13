c4a2374538ea6e9b7ecca6a16be3d136001fe8f4
Areum Jo <areumjo1@gmail.com>
2019-08-13 15:16:11 -0400

    Step2 completed: set up state in Todolist component and redered it

--- todo-reducer/src/components/Todolist.js
+++ todo-reducer/src/components/Todolist.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
+import { initialState, todoReducer } from '../reducers/todoReducer.js';
 
 const Todolist = () => {
+    const [todo, setTodo] = useState();
+    const [state, dispatch] = useReducer(todoReducer, initialState);
+    
+    console.log(state);
+    
     return(
-        <div>
-            testing
+        <div className="todo-list">
+            <h1>To-do list</h1>
+            <p>{state.item}</p>
         </div>
     )
 }
